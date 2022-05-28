f9d0841899c3237fce7972dc76b27b5e42a35f4b
Your Name <you@example.com>
2022-05-28 21:08:18 +1000

    Обновление

--- java/ru/wiweb/StringUtils.java
+++ java/ru/wiweb/StringUtils.java
@@ -1,45 +1,59 @@
-package ru.wiweb;
 
-/**
- * Строковые утилиты
- * Тестовые задания здесь же
- */
-public class StringUtils {
+    package org.test;
 
     /**
-     * Получить название дня недели по его номеру
-     *
-     * @param weekDayNumber номер дня недели
-     * @return
+     * Строковые утилиты
+     * Тестовые задания здесь же
      */
-    public static String getWeekDayName(int weekDayNumber){
-        String dayOfWeek = "";
+    public class StringUtils {
 
-        switch (weekDayNumber) {
-            case 1:
-                dayOfWeek = "понедельник";
-                break;
-            case 2:
-                dayOfWeek = "вторник";
-                break;
-            case 3:
-                dayOfWeek = "среда";
-                break;
-            case 4:
-                dayOfWeek = "четверг";
-                break;
-            case 5:
-                dayOfWeek = "пятница";
-                break;
-            case 6:
-                dayOfWeek = "суббота";
-                break;
-            case 7:
-                dayOfWeek = "воскресенье";
-                break;
-            default:
-                dayOfWeek = "не определенно!";
+        public static String getmonthName(int monthNumber){
+            String month = "";
+
+            switch (monthNumber) {
+                case 1:
+                    month = "январь";
+                    break;
+                case 2:
+                    month = "февраль";
+                    break;
+                case 3:
+                    month = "март";
+                    break;
+                case 4:
+                    month = "апрель";
+                    break;
+                case 5:
+                    month = "май";
+                    break;
+                case 6:
+                    month = "июнь";
+                    break;
+                case 7:
+                    month = "июль";
+                    break;
+                case 8:
+                    month = "август";
+                    break;
+                case 9:
+                    month = "сентябрь";
+                    break;
+                case 10:
+                    month = "октябрь";
+                    break;
+                case 11:
+                    month = "ноябрь";
+                    break;
+                case 12:
+                    month = "декабрь";
+                    break;
+                default:
+                    month = "не определенно!";
+                    break;
+
+            }
+            return month;
         }
-        return dayOfWeek;
     }
-}
+
+
